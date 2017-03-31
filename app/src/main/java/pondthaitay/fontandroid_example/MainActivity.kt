61bc02d4e8695c22acdf69aafa4eaa2d2513dec2
jedsada <jt@20scoops.com>
2017-03-31 19:01:06 +0700

    font example.

--- app/src/main/java/pondthaitay/fontandroid_example/MainActivity.kt
+++ app/src/main/java/pondthaitay/fontandroid_example/MainActivity.kt
@@ -1,5 +1,19 @@
 package pondthaitay.fontandroid_example
 
-class MainActivity : BaseActivity() {
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if (id == R.id.tv_test) startActivity(Intent(this, SecondActivity::class.java))
+    }
 }
--- app/src/main/java/pondthaitay/fontandroid_example/SecondActivity.kt
+++ app/src/main/java/pondthaitay/fontandroid_example/SecondActivity.kt
@@ -3,9 +3,10 @@ package pondthaitay.fontandroid_example
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-open class BaseActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
     }
 }
