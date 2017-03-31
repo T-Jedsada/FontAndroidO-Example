8bfb811473aa24db4a4bd346d00f7f6e83a6c869
jedsada <jt@20scoops.com>
2017-03-31 18:09:24 +0700

    add font family.

--- app/src/main/java/pondthaitay/fontandroid_example/MainActivity.kt
+++ app/src/main/java/pondthaitay/fontandroid_example/MainActivity.kt
@@ -1,12 +1,5 @@
 package pondthaitay.fontandroid_example
 
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+class MainActivity : BaseActivity() {
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
 }
