38248f71fba0124a10d12aab4d8d4032e4400c16
jedsada <jt@20scoops.com>
2017-04-03 16:06:09 +0700

    update code.

--- app/src/main/java/pondthaitay/fontandroid_example/MainActivity.kt
+++ app/src/main/java/pondthaitay/fontandroid_example/MainActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val typeface = resources.getFont(R.font.kanit_bold)
+        ((findViewById(R.id.tv_test)) as TextView).typeface = typeface
     }
 
     override fun onClick(view: View) {
